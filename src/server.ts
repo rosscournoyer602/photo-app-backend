6fbc1501b7984b20df236783583e09696ed70d81
Ross Cournoyer <ross@mediasia-interactive.com>
2021-02-28 22:28:40 +0800

    prepare to deploy

--- src/server.ts
+++ src/server.ts
@@ -14,8 +14,18 @@ import './controllers/personController'
 import './controllers/photoController'
 import { Person } from './entity/Person'
 import { User } from './entity/User'
+import { Photo } from './entity/Photo'
 
-createConnection().then(() => {
+createConnection({
+  type: 'postgres',
+  entities: [Photo, Person, User],
+  port: 5432,
+  synchronize: true,
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+}).then(() => {
   const app = express()
   app.use(cors())
   app.use(morgan(':method :url :status :response-time ms'))
